37ab9f43cdc1da752c69a48aa7a3c97fa5e19d30
darkcorp <darkcorpd@gmail.com>
2022-08-12 23:56:34 +0300

    complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor(minValue, maxValue) {
+        this.minValue;
+        this.maxValue;
+      }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        return Math.round((this.maxValue + this.minValue) / 2);
     }
 
     lower() {
-
+        this.maxValue = Math.round((this.maxValue + this.minValue) / 2);
     }
 
     greater() {
-
+        this.minValue = Math.round((this.maxValue + this.minValue) / 2);
     }
 }
 
